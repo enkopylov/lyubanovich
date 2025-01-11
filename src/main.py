d8ad65dacb2458f4a3646568f8534243046c9bf8
evgkopylov <89000917241@mail.ru>
2025-01-11 22:16:52 +0500

    add route echo

--- src/main.py
+++ src/main.py
@@ -9,5 +9,10 @@ def top():
     return 'Top here'
 
 
+@app.get('/echo/{thing}')
+def echo(thing):
+    return f'echoing {thing}'
+
+
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
